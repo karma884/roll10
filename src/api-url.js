088d808e468b075db0f651e6c4f9235d8f22dbcf
karma884 <ray.hirsh@gmail.com>
2022-02-04 09:13:34 +0200

    auto change url

--- src/api-url.js
+++ src/api-url.js
@@ -1,3 +1,4 @@
 //const url = "https://roll10-api.herokuapp.com/";
-const url = "http://localhost:5000/";
+//const url = "http://localhost:5000/";
+const url = process.env.ROLL10_API_URL || "http://localhost:5000/";
 export default url;
